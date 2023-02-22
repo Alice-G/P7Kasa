d333882992d567d7d25ea83e7cf7845ab8b8011f
Alice-G <38867948+Alice-G@users.noreply.github.com>
2023-02-22 17:21:32 +0100

    Adjust index

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 // import ReactDOM from 'react-dom' // not supported anymore?
-import ReactDOM from 'react-dom/client' //    ASK which one?
-// import './index.css' // DEL
+import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import { BrowserRouter } from 'react-router-dom'
-// BrowserRouter for the web and NativeRouter for mobile // ASK how I switch for mobile views
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
